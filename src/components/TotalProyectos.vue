24229f12b65f296161d360e841eb488605b9cb9c
YouGiovanni <a3521110570@alumno.uttehuacan.edu.mx>
2022-10-12 23:03:34 -0500

    Quinta actualizacion de Formulario

---
+++ src/components/TotalProyectos.vue
@@ -0,0 +1,42 @@
+<template>
+        <div class="row">
+        <div class="col-6 my-2">
+            <h3>Total proyectos: {{ numeroProyectos }}</h3>
+        </div>
+        <div class="col-6 test-end">
+           <button @click="limpiarData" class="btn btn-warning"> Limpiar Proyectos</button> 
+        </div>
+    </div>
+
+
+    <div class="table-responsive">
+            <table class="table table-dark">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>proyecto</th>
+                        <th>Tipo</th>
+                        <th>Urgente</th>
+                        <th>Completado</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr v-for="(proyecto,index) in proyectos" :key="index">
+                        <td>{{ index+1 }}</td>
+                        <td>{{ proyecto.proyecto }}</td>
+                        <td>{{proyecto.tipo }}</td>
+                        <td @click="CambiarEstado(proyecto, 'urgente')" :class="proyecto.urgente ? 'bg-success' : 'bg-danger'">{{ proyecto.urgente ? "Si" : "No"}}</td>
+                        <td @click="CambiarEstado(proyecto, 'completado')" :class="proyecto.completado ? 'bg-success' : 'bg-danger'">{{ proyecto.completado ? "Completo" : "Incompleto"}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <hr>
+</template>
+
+<script>
+    export default{
+        props:['numeroProyectos', 'proyectos', 'CambiarEstado','limpiarData'],
+    };
+</script>
